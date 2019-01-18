ad7232c2a4db04a8ec6a6a4518c9031d9c316cc8
9dmccormack <31480975+9dmccormack@users.noreply.github.com>
2019-01-18 11:36:58 -0600

    Add files via upload

--- exercises_6-9.sql
+++ exercises_6-9.sql
@@ -38,4 +38,44 @@ FROM buildings
 WHERE role IS NULL;
 
 --exercise 9
+SELECT title, (domestic_sales + international_sales) / 1000000 AS gross_sales_millions
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
 
+SELECT title, rating * 10 AS rating_percent
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+
+SELECT title, year
+FROM movies
+WHERE year % 2 = 0;
+
+--exercise 10
+SELECT MAX(years_employed) as Max_years_employed
+FROM employees;
+
+SELECT role, AVG(years_employed) as Average_years_employed
+FROM employees
+GROUP BY role;
+
+SELECT building, SUM(years_employed) as Total_years_employed
+FROM employees
+GROUP BY building;
+
+--exercise 11
+SELECT role, COUNT(*) as Number_of_artists
+FROM employees
+WHERE role = "Artist";
+
+SELECT role, COUNT(*)
+FROM employees
+GROUP BY role;
+
+SELECT role, SUM(years_employed)
+FROM employees
+GROUP BY role
+HAVING role = "Engineer";
+
+--exercise 12
